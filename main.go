9e4c64b244c8b3a9b2642565fbef20586227478e
Togami Yuki <togamin@TogaminoMacBook-Air.local>
2022-06-12 20:00:06 +0900

    関数切り出しリファクタリング

--- main.go
+++ main.go
@@ -16,14 +16,17 @@ func Hello(name string, language string) string {
 	if name == ""{
 		name = "World"
 	}
+	return greetingPrefix(language) + name
+}
 
-	prefix := englishPrefix
-
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case japanese :
 		prefix = janpanesePrefix
 	case french :
 		prefix = frenchPrefix
+	default :
+		prefix = englishPrefix
 	}
-	return prefix + name
+	return prefix
 }
